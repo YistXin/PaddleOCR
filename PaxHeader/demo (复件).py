7c9a657fe6648f744b024699dbf1e87e789ecec9
Yist <731749207@qq.com>
2021-07-12 18:51:57 +0800

    paddle recognize words

--- PaxHeader/demo (复件).py
+++
@@ -1,3 +0,0 @@
-72 path=moveit_ws/src/moveit/visual_grab/src/paddleocr/demo (复件).py
-27 mtime=1623046176.773397
-27 atime=1623053858.396913
--- PaxHeader/ocr_pose (复件).py
+++
@@ -1,3 +0,0 @@
-76 path=moveit_ws/src/moveit/visual_grab/src/paddleocr/ocr_pose (复件).py
-26 mtime=1622857895.03954
-27 atime=1622858414.792981
